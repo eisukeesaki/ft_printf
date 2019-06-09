aa32a1b8e6b1352b735fc92499b0ffe44345e3ea
Eisuke Esaki <eesaki@student.42.us.org>
2019-06-08 20:23:23 -0700

    handles %s, %d, %i, %c, %u

--- ft_printf.c
+++ ft_printf.c
@@ -3,48 +3,47 @@
 #include <stdarg.h>
 #include "libft/libft.h"
 
-size_t	ft_count_substr(const char *s, unsigned int start, char end)
-{
-	size_t	len;
-
-	if (!s || !end)
-		return (0);
-
-	len = 0;
-	while (s[start + len] != end && s[start + len])
-		len++;;
-	return (len);
-}
-
-void	ft_print_substr(const char *s, unsigned int start, size_t len)
-{
-	size_t	i;
-
-	if (!s || len < 1)
-		return ;
-
-	i = 0;
-	while (i < len)
-	{
-		write(1, &s[start + i], 1);
-		i++;;
-	}
-}
+void	ft_putunbr(unsigned int n);
 
 int		ft_printf(const char *s, ...)
 {
 	size_t	i;
-	size_t	len;
 	va_list	ap;
 
 	i = 0;
-	len = 0;
 	va_start(ap, s);
 	while (s[i])
 	{
-		len = ft_count_substr(s, i, '%');
-		ft_print_substr(s, i, len);
-		i += len + 1;
+		if (s[i] == '%')
+		{
+			if (s[i + 1] == 'd' || s[i + 1] == 'i')
+			{
+				int arg = va_arg(ap, int);
+				ft_putnbr(arg);
+				i++;
+			}
+			else if (s[i + 1] == 's')
+			{
+				char *arg = va_arg(ap, char *);
+				ft_putstr(arg);
+				i++;
+			}
+			else if (s[i + 1] == 'c')
+			{
+				char arg = va_arg(ap, int);
+				write(1, &arg, 1);
+				i++;
+			}
+			else if (s[i + 1] == 'u')
+			{
+				char arg = va_arg(ap, unsigned int);
+				ft_putunbr(arg);
+				i++;
+			}
+		}
+		else
+			write(1, &s[i], 1);
+		i++;
 	}
 	va_end(ap);
 
@@ -53,11 +52,13 @@ int		ft_printf(const char *s, ...)
 
 int		main(void)
 {
-	// const char	s[] = "def";
-	// int			n = 2147483647;
+	const char		s[] = "apple";
+	int				d = -2147483648;
+	char			c = 'A';
+	unsigned int	u = 4294967295;
 
-	// ft_printf("abc%sghi%djkl", s, n);
-	ft_printf("moulinette%is%trash");
+	// ft_printf("This %s costs %d dollars%c uint:%u", s, n, c, un);
+	ft_printf("string:%s\nint:%d\nchar:%c\nunsigned int:%u\n", s, d, c, u);
 
 	return (0);
 }
@@ -76,3 +77,53 @@ int		main(void)
 		// 	write(1, "\n", 1);
 		// }
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> back up
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< back up
+// size_t	ft_count_substr(const char *s, unsigned int start, char end)
+// {
+// 	size_t	len;
+
+// 	if (!s || !end)
+// 		return (0);
+
+// 	len = 0;
+// 	while (s[start + len] != end && s[start + len])
+// 		len++;
+// 	return (len);
+// }
+
+// void	ft_print_substr(const char *s, unsigned int start, size_t len)
+// {
+// 	size_t	i;
+
+// 	if (!s || len < 1)
+// 		return ;
+
+// 	i = 0;
+// 	while (i < len)
+// 	{
+// 		write(1, &s[start + i], 1);
+// 		i++;;
+// 	}
+// }
+
+// int		ft_printf(const char *s, ...)
+// {
+// 	size_t	i;
+// 	size_t	len;
+// 	va_list	ap;
+
+// 	i = 0;
+// 	len = 0;
+// 	va_start(ap, s);
+// 	while (s[i])
+// 	{
+// 		len = ft_count_substr(s, i, '%');
+// 		ft_print_substr(s, i, len);
+// 		i += len + 1;
+// 	}
+// 	va_end(ap);
+
+// 	return (0);
+// }
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> backup
