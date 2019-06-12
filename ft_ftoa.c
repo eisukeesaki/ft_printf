868b721e174bbf3ac6e1574afd500a550c10aefa
Eisuke Esaki <eesaki@student.42.us.org>
2019-06-12 00:54:28 -0700

    restrucuring ftoa and ftol

--- ft_ftoa.c
+++ ft_ftoa.c
@@ -13,72 +13,96 @@
 #include "libft/libft.h"
 #include <stdio.h>
 
-size_t	ft_count_fdigit(float n)
+long	ft_ftol(float n, size_t *dgt, size_t *dp, unsigned int *neg)
 {
-	size_t	i;
+	float	f;
 	long	nb;
 
-	i = 1;
-	while (n / (int)n != 1)
+	f = n;
+	*neg = f < 0 ? 1 : 0;
+	if (f < 0)
+		f *= -1;
+	while (f / (int)f != 1) // *1
 	{
-		n = n * 10;
+		f = f * 10;
+		(*dp)++;
 	}
-	nb = (long)n;
+	nb = (long)f;
 	while (nb >= 10)
 	{
-		i++;
+		(*dgt)++;
 		nb /= 10;
 	}
-	return (i);
+	return (f);
 }
 
-char	*ft_ftoa(int n)
-{
-	long		nb;
-	size_t		dgt;
-	char		*s;
-	unsigned	neg;
+/*
+TODO
+- change *1's loop condition to absolute precision
+- redesign algorithm to find dp position
+- store all digits (absolute precision), sign, dp to array
+*/
 
-	nb = n;
-	neg = n < 0 ? 1 : 0;
-	dgt = ft_count_digit(nb);
-	s = ft_strnew(dgt + neg);
+char	*ft_ftoa(float n)
+{
+	long			nb;
+	size_t			dgt;
+	size_t			dp;
+	char			*s;
+	unsigned int	neg;
+
+	dgt = 1;
+	dp = 0;
+	nb = ft_ftol(n, &dgt, &dp, &neg);
+	s = ft_strnew(dgt + neg + 1);
+	dp = dgt - dp;
+	dgt++;
 	if (!s)
 		return (NULL);
 	if (neg)
 	{
-		nb = -nb;
 		s[0] = '-';
+		dgt++;
+		dp++;
 	}
+	s[dp] = '.';
 	while (dgt > 0)
 	{
-		s[dgt + neg - 1] = (nb % 10) + '0';
-		dgt--;
-		nb /= 10;
+		if (s[dgt - 1] == '.')
+			dgt--;
+		else
+		{
+			s[dgt + neg - 1] = (nb % 10) + '0';
+			dgt--;
+			nb /= 10;
+		}
 	}
 	return (s);
 }
 
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< ftoa
-// int		main(void)
-// {
-// 	float	f = 123.4567;
-
-// 	ft_putstr(ft_ftoa(f));
-
-// 	return (0);
-// }
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ftoa
-
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< count digit
 int		main(void)
 {
-	float	f = 123.4567;
+	float	f = -123.456;
+	printf("mine:%s\n", ft_ftoa(f));
 
-	printf("ft_count_digit:%lu\n", ft_count_fdigit(f));
+	// double	f2 = 123456789.123456;
+	// printf("libc:%lf\n", f2);
 
 	return (0);
 }
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ftoa
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< count digit
+// int		main(void)
+// {
+// 	float	f = 123.4567;
+// 	size_t	dgt = 0;
+
+// 	printf("ft_count_digit:%lu\n", ft_ftol(f, dgt));
+
+// 	return (0);
+// }
 
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< original itoa
 // char	*ft_itoa(int n)
