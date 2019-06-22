482ada8c7ec9333c0877d3a927c620b5d0165771
eisuke <izkyyy@gmail.com>
2019-06-21 20:24:23 -0700

    created ft_putfloat() and main with test cases for it

---
+++ ft_putfloat.c
@@ -0,0 +1,10 @@
+#include "libft/libft.h"
+
+void	ft_putdouble(double n)
+{
+	long	int_part;
+	int		frac_part;
+
+	int_part = (long)n;
+	frac_part = n - ()
+}
---
+++ main_ft_putfloat.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+
+int			main(void)
+{
+	// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< positive
+	// ft_putstr("mine:");
+	// double	f = 789.123456;
+	// ft_putdouble(f);
+	// ft_putstr("\n");
+
+	// double	f2 = 789.123456;
+	// printf("libc:%lf\n", f2);
+	// ft_putstr("\n");
+
+	// f = 0.123456;
+	// ft_putstr("mine:");
+	// ft_putdouble(f);
+	// ft_putstr("\n");
+
+	// f2 = 0.123456;
+	// printf("libc:%lf\n", f2);
+	// ft_putstr("\n");
+
+	// f = 1.123456;
+	// ft_putstr("mine:");
+	// ft_putdouble(f);
+	// ft_putstr("\n");
+
+	// f2 = 1.123456;
+	// printf("libc:%lf\n", f2);
+	// ft_putstr("\n");
+
+	// f = 1234567891.123456;
+	// ft_putstr("mine:");
+	// ft_putdouble(f);
+	// ft_putstr("\n");
+
+	// f2 = 1234567891.123456;
+	// printf("libc:%lf\n", f2);
+	// ft_putstr("\n");
+
+	// f = 1020304050.060708;
+	// ft_putstr("mine:");
+	// ft_putdouble(f);
+	// ft_putstr("\n");
+
+	// f2 = 1020304050.060708;
+	// printf("libc:%lf\n", f2);
+	// ft_putstr("\n");
+
+	// f = 9223372036854.123456;
+	// ft_putstr("mine:");
+	// ft_putdouble(f);
+	// ft_putstr("\n");
+
+	// f2 = 9223372036854.123456;
+	// printf("libc:%lf\n", f2);
+	// ft_putstr("\n");
+
+	double	f = 9223372036854775807.123456;
+	ft_putstr("mine:");
+	ft_putdouble(f);
+	ft_putstr("\n");
+
+	double	f2 = 9223372036854775807.123456;
+	printf("libc:%lf\n", f2);
+	ft_putstr("\n");
+	// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> positive
+
+	// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 0
+	// f = 0;
+	// ft_putstr("mine:");
+	// ft_putdouble(f);
+	// ft_putstr("\n");
+
+	// f2 = 0;
+	// printf("libc:%lf\n", f2);
+	// ft_putstr("\n");
+	// // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 0
+
+	// // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< negative
+	// ft_putstr("mine:");
+	// f = -789.123456;
+	// ft_putdouble(f);
+	// ft_putstr("\n");
+
+	// f2 = -789.123456;
+	// printf("libc:%lf\n", f2);
+	// ft_putstr("\n");
+
+	// f = -0.123456;
+	// ft_putstr("mine:");
+	// ft_putdouble(f);
+	// ft_putstr("\n");
+
+	// f2 = -0.123456;
+	// printf("libc:%lf\n", f2);
+	// ft_putstr("\n");
+
+	// f = -1.23456;
+	// ft_putstr("mine:");
+	// ft_putdouble(f);
+	// ft_putstr("\n");
+
+	// f2 = -1.23456;
+	// printf("libc:%lf\n", f2);
+	// ft_putstr("\n");
+
+	// f = -1234567891.123456;
+	// ft_putstr("mine:");
+	// ft_putdouble(f);
+	// ft_putstr("\n");
+
+	// f2 = -1234567891.123456;
+	// printf("libc:%lf\n", f2);
+	// ft_putstr("\n");
+
+	// f = -1020304050.060708;
+	// ft_putstr("mine:");
+	// ft_putdouble(f);
+	// ft_putstr("\n");
+
+	// f2 = -1020304050.060708;
+	// printf("libc:%lf\n", f2);
+	// ft_putstr("\n");
+
+	// f = -9223372036854775807.123456;
+	// ft_putstr("mine:");
+	// ft_putdouble(f);
+	// ft_putstr("\n");
+
+	// f2 = -9223372036854775807.123456;
+	// printf("libc:%lf\n", f2);
+	// ft_putstr("\n");
+	// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> negative
+
+	return (0);
+}
