0c14de552193d45265b53a862d20d202fde32d2e
Eisuke Esaki <eesaki@student.42.us.org>
2019-06-21 23:26:03 -0700

    removed comments

--- ftoa.c
+++ ftoa.c
@@ -1,74 +1,55 @@
-// C program for implementation of ftoa() 
-#include<stdio.h> 
-#include<math.h> 
-  
-// reverses a string 'str' of length 'len' 
-void reverse(char *str, int len) 
-{ 
-    int i=0, j=len-1, temp; 
-    while (i<j) 
-    { 
-        temp = str[i]; 
-        str[i] = str[j]; 
-        str[j] = temp; 
-        i++; j--; 
-    } 
-} 
-  
- // Converts a given integer x to string str[].  d is the number 
- // of digits required in output. If d is more than the number 
- // of digits in x, then 0s are added at the beginning. 
-int intToStr(int x, char str[], int d) 
-{ 
-    int i = 0; 
-    while (x) 
-    { 
-        str[i++] = (x%10) + '0'; 
-        x = x/10; 
-    } 
-  
-    // If number of digits required is more, then 
-    // add 0s at the beginning 
-    while (i < d) 
-        str[i++] = '0'; 
-  
-    reverse(str, i); 
-    str[i] = '\0'; 
-    return i; 
-} 
-  
-// Converts a floating point number to string. 
-void ftoa(float n, char *res, int afterpoint) 
-{ 
-    // Extract integer part 
-    int ipart = (int)n; 
-  
-    // Extract floating part 
-    float fpart = n - (float)ipart; 
-  
-    // convert integer part to string 
-    int i = intToStr(ipart, res, 0); 
-  
-    // check for display option after point 
-    if (afterpoint != 0) 
-    { 
-        res[i] = '.';  // add dot 
-  
-        // Get the value of fraction part upto given no. 
-        // of points after dot. The third parameter is needed 
-        // to handle cases like 233.007 
-        fpart = fpart * pow(10, afterpoint); 
-  
-        intToStr((int)fpart, res + i + 1, afterpoint); 
-    } 
-} 
-  
-// driver program to test above funtion 
-int main() 
-{ 
-    char res[20]; 
-    float n = 100.000456; 
-    ftoa(n, res, 6); 
-    printf("\n\"%s\"\n", res); 
-    return 0; 
+#include<stdio.h>
+#include<math.h>
+
+void reverse(char *str, int len)
+{
+    int i=0, j=len-1, temp;
+    while (i<j)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++; j--;
+    }
+}
+
+int intToStr(int x, char str[], int d)
+{
+    int i = 0;
+
+    while (x)
+    {
+        str[i++] = (x%10) + '0';
+        x = x/10;
+    }
+    while (i < d)
+        str[i++] = '0';
+    reverse(str, i);
+    str[i] = '\0';
+
+    return i;
+}
+
+void ftoa(float n, char *res, int afterpoint)
+{
+    int ipart = (int)n;
+    float fpart = n - (float)ipart;
+    int i = intToStr(ipart, res, 0);
+
+    if (afterpoint != 0)
+    {
+        res[i] = '.';
+        fpart = fpart * pow(10, afterpoint);
+        intToStr((int)fpart, res + i + 1, afterpoint);
+    }
+}
+
+int main()
+{
+    char res[100];
+    double n = 9223372036854.123456;
+    ftoa(n, res, 6);
+    printf("\n\"%s\"\n", res);
+    printf("libc:%f\n", 9223372036854.123456);
+    return 0;
 }
