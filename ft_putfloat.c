4575f952643202af37f1250494325ec79fafebd4
Eisuke Esaki <eesaki@student.42.us.org>
2019-06-25 02:37:44 -0700

    can convert int part if nbr is in range of long

--- ft_putfloat.c
+++ ft_putfloat.c
@@ -1,77 +1,99 @@
+#include <stdio.h>
 #include "libft/libft.h"
 
-double	ft_pow(double base, double exponent);
-
-size_t	ft_count_digit(long n)
+long double	ft_fmod(long double n, long double div)
 {
-	size_t	i;
-
-	i = 1;
-	if (n < 0)
-		n = -n;
-	while (n >= 10)
-	{
-		i++;
-		n /= 10;
-	}
-	return (i);
+	return (n - div * (long)(n / div)); // use of type casting becomes a bottleneck
 }
 
-void	reverse_array(char *s, size_t len)
+char	*reverse_str(char *s, size_t len)
 {
-	size_t	head;
-	size_t	tail;
+	size_t	i;
+	size_t	k;
 	char	tmp;
 
-	head = 0;
-	tail = len - 1;
-	while (head < tail)
-	{
-		tmp = s[head];
-		s[head] = s[tail];
-		s[tail] = tmp;
-		head++;
-		tail--;
-	}
-}
-
-void	convert_to_str(long part, char *s)
-{
-	size_t	i;
-
 	i = 0;
-	while (part)
+	k = len - 1;
+	while (i < k)
 	{
-		s[i++] = part % 10 + '0';
-		part /= 10;
-	}
-	reverse_array(s, i);
-	s[i] = '\0';
-	return (i);
-}
-
-void	ft_putfloat(double n, size_t prec)
-{
-	long	int_part;
-	double	frac_part;
-	char	*s;
-	size_t	i;
-
-	int_part = (long)n;
-	frac_part = n - (double)int_part;
-
-	if (!(s = ft_strnew(ft_count_digit(int_part) + prec + 1)))
-		return ;
-
-	i = 0;
-	convert_to_str(int_part, s, &i);
-
-	if (prec != 0)
-	{
-		s[i] = '.';
+		tmp = s[i];
+		s[i] = s[k];
+		s[k] = tmp;
 		i++;
-		frac_part = frac_part * ft_pow(10, prec);
-		convert_to_str((long)frac_part, s, &i);
+		k--;
 	}
-	ft_putstr(s);
+	return (s);
+}
+
+char	*assign_int_part(long double n, char *s, size_t len, int neg)
+{
+	size_t	i;
+
+	if ((i = neg))
+		s[i] = '-';
+	while (i < len)
+	{
+		if (s[i] == '.')
+			i++;
+		else
+		{
+			s[i] = ((int)ft_fmod(n, 10)) + '0';
+			n /= 10;
+			i++;
+		}
+	}
+	s = reverse_str(s, len);
+	s[i] = '.';
+	return (s);
+}
+
+char	*assign_dp(size_t slen, size_t prec, char *s, int neg)
+{
+	s[(slen - 1) - prec + neg] = '.';
+	return (s);
+}
+
+size_t	ct_int_part(long double n)
+{
+	size_t	len;
+
+	len = 0;
+	while (n > 1)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+int		check_neg(long double n)
+{
+	return (n < 0 ? 1 : 0);
+}
+
+void	ft_putfloat(long double n, size_t prec)
+{
+	char	*int_part;
+	// char	*frac_part;
+	size_t	len;
+	int		neg;
+
+	neg = check_neg(n);
+	len = ct_int_part(n);
+	int_part = ft_strnew(neg + len + 1);
+	int_part = assign_int_part(n, int_part, len, neg);
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< debug
+	printf("<dbg%lu>\n", prec);
+	ft_putstr(int_part);
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> debug
+}
+
+int		main(void)
+{
+	// long double	n = 123.456789;
+	long double	n = 1234567890123456789.123456;
+
+	ft_putfloat(n, 6);
+
+	return (0);
 }
