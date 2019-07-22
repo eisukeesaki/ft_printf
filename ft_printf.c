b8b5d7261a92bc02bdf80e5eff3db14e84d9da88
Eisuke Esaki <eesaki@student.42.us.org>
2019-07-21 21:51:21 -0700

    implemented jump table

--- ft_printf.c
+++ ft_printf.c
@@ -2,8 +2,18 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include "libft/libft.h"
+#include "ft_printf.h"
 
-void	ft_putunbr(unsigned int n);
+void	ft_putfloat(long double n, size_t prec);
+
+printer *g_jump_table[256] =
+{
+	['d'] = &dispatch_d,
+	['c'] = &dispatch_c,
+	['s'] = &dispatch_s,
+	['f'] = &dispatch_f,
+	['u'] = &dispatch_u,
+};
 
 int		ft_printf(const char *s, ...)
 {
@@ -16,36 +26,9 @@ int		ft_printf(const char *s, ...)
 	{
 		if (s[i] == '%')
 		{
-			if (s[i + 1] == 'd' || s[i + 1] == 'i')
+			if (g_jump_table[(unsigned char)s[i + 1]])
 			{
-				int arg = va_arg(ap, int);
-				ft_putnbr(arg);
-				i++;
-			}
-			else if (s[i + 1] == 's')
-			{
-				char *arg = va_arg(ap, char *);
-				ft_putstr(arg);
-				i++;
-			}
-			else if (s[i + 1] == 'c')
-			{
-				char arg = va_arg(ap, int);
-				write(1, &arg, 1);
-				i++;
-			}
-			else if (s[i + 1] == 'u')
-			{
-				char arg = va_arg(ap, unsigned int);
-				ft_putunbr(arg);
-				i++;
-			}
-			else if (s[i + 1] == 'f')
-			{
-				double arg = va_arg(ap, double);
-				char *s = ft_ftoa(arg);
-				ft_putstr(s);
-				free(s);
+				g_jump_table[(unsigned char)s[i + 1]](ap);
 				i++;
 			}
 		}
@@ -64,75 +47,9 @@ int		main(void)
 	int				d = -2147483648;
 	char			c = 'A';
 	unsigned int	u = 4294967295;
-	float			f = 123.45678;
+	long double		f = 1234567891.123456;
 
 	ft_printf("string:%s\nint:%d\nchar:%c\nunsigned int:%u\nfloat:%f\n", s, d, c, u, f);
-	printf("libc:%f\n", 1234567891234.123456);
 
 	return (0);
 }
-
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< back up
-		// if (*(find_fmt_spec(s)) == 's')
-		// {
-		// 	char *arg = va_arg(ap, char *);
-		// 	ft_putstr(arg);
-		// 	write(1, "\n", 1);
-		// }
-		// else if (*(find_fmt_spec(s)) == 'd')
-		// {
-		// 	int arg = va_arg(ap, int);
-		// 	ft_putnbr(arg);
-		// 	write(1, "\n", 1);
-		// }
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> back up
-
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< back up
-// size_t	ft_count_substr(const char *s, unsigned int start, char end)
-// {
-// 	size_t	len;
-
-// 	if (!s || !end)
-// 		return (0);
-
-// 	len = 0;
-// 	while (s[start + len] != end && s[start + len])
-// 		len++;
-// 	return (len);
-// }
-
-// void	ft_print_substr(const char *s, unsigned int start, size_t len)
-// {
-// 	size_t	i;
-
-// 	if (!s || len < 1)
-// 		return ;
-
-// 	i = 0;
-// 	while (i < len)
-// 	{
-// 		write(1, &s[start + i], 1);
-// 		i++;;
-// 	}
-// }
-
-// int		ft_printf(const char *s, ...)
-// {
-// 	size_t	i;
-// 	size_t	len;
-// 	va_list	ap;
-
-// 	i = 0;
-// 	len = 0;
-// 	va_start(ap, s);
-// 	while (s[i])
-// 	{
-// 		len = ft_count_substr(s, i, '%');
-// 		ft_print_substr(s, i, len);
-// 		i += len + 1;
-// 	}
-// 	va_end(ap);
-
-// 	return (0);
-// }
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> backup
