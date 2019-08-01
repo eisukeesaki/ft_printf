16874a899b5afc5dcce1d930b053f3c202965efb
Eisuke Esaki <eesaki@student.42.us.org>
2019-07-31 21:18:46 -0700

    added prototypes for %o, %X, %x

--- ft_printf.h
+++ ft_printf.h
@@ -7,6 +7,9 @@ void			dispatch_c(va_list ap);
 void			dispatch_s(va_list ap);
 void			dispatch_f(va_list ap);
 void			dispatch_u(va_list ap);
+void			dispatch_o(va_list ap);
+void			dispatch_X(va_list ap);
+void			dispatch_x(va_list ap);
 
 typedef void	printer(va_list);
 
