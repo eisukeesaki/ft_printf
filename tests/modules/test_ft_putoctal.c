517a3c2357c0296484f01968a46e6fed9ec5f409
Eisuke Esaki <eesaki@student.42.us.org>
2019-07-31 21:10:06 -0700

    implemented tests for min/max values of different data types

--- tests/modules/test_ft_putoctal.c
+++ tests/modules/test_ft_putoctal.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
+#include <limits.h>
 #include "../../libft/libft.h"
 
-void		ft_putoctal(int d);
+void		ft_putoctal(unsigned long long d);
 
 int		main(void)
 {
-	// int	d_libc = 0;
-	// int	d_mine = 0;
-	int	d = 2147483647;
+	// int						n = 0;
 
-	// while (d < 128)
+	// short				n = SHRT_MIN;
+	// short				n = SHRT_MAX;
+	// unsigned short		n = USHRT_MAX;
+
+	// int					n = INT_MIN;
+	// int					n = INT_MAX;
+	// unsigned			n = UINT_MAX;
+
+	// long				n = LONG_MIN;
+	// long				n = LONG_MAX;
+	// unsigned long		n = ULONG_MAX;
+
+	// long long			n = LLONG_MIN;
+	// long long			n = LONG_MAX;
+	unsigned long long	n = ULLONG_MAX;
+
+	// while (n < 100)
 	// {
-	// 	printf("libc:%o\n", d);
-	// 	ft_putstr("mine:");
-	// 	ft_putoctal(d);
-	// 	printf("\n");
-	// 	printf("\n");
-	// 	d++;
+		printf("libc:%llo\n", n);
+		ft_putstr("mine:");
+		ft_putoctal(n);
+		ft_putstr("\n\n");
+	// 	n++;
 	// }
-
-	printf("libc:%o\n", d);
-	ft_putstr("mine:");
-	ft_putoctal(d);
 
 	return (0);
 }
