2c50efd46dad0c7fd4355b4d2e9efeb793b3dbab
Eisuke Esaki <eesaki@student.42.us.org>
2019-09-03 05:54:50 -0700

    minor changes

--- tests/debug_d.c
+++ tests/debug_d.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <limits.h>
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> test purpose
-#include "../libft/libft.h"
-#include "../ft_printf.h"
+#include "../../ft_printf/libft/libft.h"
+#include "../../ft_printf/ft_printf.h"
 
 int		ft_printf(const char *format, ...);
 
@@ -14,13 +14,14 @@ int		main(void)
 
 	// setbuf(stdout, NULL);
 
-	// nprinted_libc = printf("arg:0, 123456|spec:*d@libc:%*d\n", 0, 123456);
-	// nprinted_mine = ft_printf("arg:0, 123456|spec:*d@mine:%*d\n", 0, 123456);
-	// printf("nprinted@libc:%d\n", nprinted_libc);
-	// printf("nprinted@mine:%d\n\n", nprinted_mine);
+	nprinted_libc = printf("%+-5.d", 0);
+	setbuf(stdout, NULL);
+	putchar('\n');
 
-	nprinted_libc = printf("arg:123456|spec:00d@libc:%009d\n", 123456);
-	nprinted_mine = ft_printf("arg:123456|spec:00d@mine:%009d\n", 123456);
+	nprinted_mine = ft_printf("%+-5.d", 0);
+	setbuf(stdout, NULL);
+	putchar('\n');
+
 	printf("nprinted@libc:%d\n", nprinted_libc);
 	printf("nprinted@mine:%d\n\n", nprinted_mine);
 
