a1779e27b61e8f59d9c9acebbe50ecdf23399c57
Eisuke Esaki <eesaki@student.42.us.org>
2019-09-29 02:01:13 -0700

    fixed where returns of write weren't added to nprinted

--- char.c
+++ char.c
@@ -6,7 +6,7 @@
 /*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/04 20:14:54 by eesaki            #+#    #+#             */
-/*   Updated: 2019/09/28 07:30:58 by eesaki           ###   ########.fr       */
+/*   Updated: 2019/09/29 01:29:28 by eesaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ void	print_char(t_format *fmt, va_list ap)
 	{
 		while (fmt->width-- > 1)
 			fmt->nprinted += write(1, " ", 1);
-		fmt->nprinted = write(1, &c, 1);
+		fmt->nprinted += write(1, &c, 1);
 	}
 	else if (fmt->width && fmt->minus)
 	{
-		fmt->nprinted = write(1, &c, 1);
+		fmt->nprinted += write(1, &c, 1);
 		while (fmt->width-- > 1)
 			fmt->nprinted += write(1, " ", 1);
 	}
 	else
-		fmt->nprinted = write(1, &c, 1);
+		fmt->nprinted += write(1, &c, 1);
 }
